b4629e8884d24880a0f43b8fe6c8759bc21d2bc9
knwins <knwin@msn.com>
2022-10-23 08:28:03 +0800

    update1

--- mobile/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbhost ="bdm818101331.my3w.com";
-$dbuser ="bdm818101331";
-$dbpass ="Szqws#2022";
-$dbbase ="bdm818101331_db";
-?>
--- mobile/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-	  <div class="copyright f26"> &copy;2015-2019 深圳市千维顺科技有限公司</div>
-</footer>
